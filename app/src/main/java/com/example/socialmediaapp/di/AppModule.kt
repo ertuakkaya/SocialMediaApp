303cd5aac7ed819b24d459281c170f9b7e4429ec
ertuakkaya <ertuakkaya@gmail.com>
2024-07-12 12:52:54 +0300

    dagger hilt setup

---
+++ app/src/main/java/com/example/socialmediaapp/di/AppModule.kt
@@ -0,0 +1,23 @@
+package com.example.socialmediaapp.di
+
+import com.google.firebase.auth.FirebaseAuth
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+class AppModule {
+
+    @Provides
+    @Singleton
+    fun provideFirebaseAuth() : FirebaseAuth{
+        return FirebaseAuth.getInstance()
+    }
+
+
+}
