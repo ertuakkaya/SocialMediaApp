689f22db51268bc2bb36df58575a5b3175453ae2
ertuakkaya <ertuakkaya@gmail.com>
2024-08-02 16:33:30 +0300

    firestore repo update for chat system

--- app/src/main/java/com/example/socialmediaapp/data/repository/FirestoreRepository.kt
+++ app/src/main/java/com/example/socialmediaapp/data/repository/FirestoreRepository.kt
@@ -1,10 +1,14 @@
 package com.example.socialmediaapp.data.repository
 
 import android.util.Log
+import com.example.socialmediaapp.data.entitiy.ChatMessage
 import com.example.socialmediaapp.data.entitiy.User
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withContext
@@ -13,6 +17,26 @@ import javax.inject.Inject
 
 class FirestoreRepository @Inject constructor(private val firestore: FirebaseFirestore){
 
+
+    /**
+     * Fetch all users from Firestore
+     */
+    suspend fun fetchUsers() : Flow<List<User>> = callbackFlow{
+        val receivedMessageListener = userCollection.addSnapshotListener{ snapshot, error ->
+            if (error != null) {
+                close(error)
+                return@addSnapshotListener
+            }
+            val users = snapshot?.toObjects(User::class.java) ?: emptyList()
+            trySend(users)
+
+        }
+        awaitClose {
+            receivedMessageListener.remove()
+        }
+
+
+    }
 
 
     fun addUserToFirestore(user: User) = firestore.collection("users").document(user.userID ?: throw IllegalArgumentException("UserID cannot be null")).set(user)
@@ -62,9 +86,71 @@ class FirestoreRepository @Inject constructor(private val firestore: FirebaseFir
     fun deleteUserFromFirestore(userID: String) = firestore.collection("users").document(userID).delete()
 
 
+    /**
+     * Real Time Chat
+     *
+     */
+
+    private val chatCollection = firestore.collection("chats")
+    private val userCollection = firestore.collection("users")
 
 
+    /**
+     *  Send Message to Firestore
+     *  @param chatMessage: ChatMessage is the message to be sent.
+     */
+
+    suspend fun sendMessage(chatMessage: ChatMessage){
+        chatCollection.add(chatMessage).await()
+    }
+
+    /**
+     * Get Chat Messages from Firestore
+     */
+
+    suspend fun getMessages(sender_id: String, receiver_id: String): Flow<List<ChatMessage>> = callbackFlow {
+        val sentMessageListener = chatCollection
+            .whereEqualTo("sender_id", sender_id)
+            .whereEqualTo("receiver_id", receiver_id)
+            .orderBy("timestamp" , Query.Direction.ASCENDING)
+            .addSnapshotListener{ snapshot, error ->
+                if (error != null) {
+                    close(error)
+                    return@addSnapshotListener
+                }
+                val messages = snapshot?.toObjects(ChatMessage::class.java) ?: emptyList()
+                trySend(messages)
+            }
+
+        val receivedMessageListener = chatCollection
+            .whereEqualTo("sender_id", receiver_id)
+            .whereEqualTo("receiver_id", sender_id)
+            .orderBy("timestamp" , Query.Direction.ASCENDING)
+            .addSnapshotListener{ snapshot, error ->
+                if (error != null) {
+                    close(error)
+                    return@addSnapshotListener
+                }
+                val messages = snapshot?.toObjects(ChatMessage::class.java) ?: emptyList()
+                trySend(messages)
+            }
 
 
+        awaitClose {
+            sentMessageListener.remove()
+            receivedMessageListener.remove()
+        }
+    }
+
+    /**
+     *  Get chat partner from Firestore
+     *  @param user_id: String is the ID of the user whose chat partner is to be fetched.
+     *  @return User? is the User object of the chat partner.
+     */
+
+    suspend fun getChatPartner(user_id: String) : User?{
+        return userCollection.document(user_id).get().await().toObject(User::class.java)
+
+    }
 
 }
